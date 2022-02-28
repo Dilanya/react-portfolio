4eacee683dc8358b42320e96f3771ea12767de54
Dilanya <96423@fhss.sjp.ac.lk>
2022-02-28 19:03:13 +0530

    create my portfolio

---
+++ src/content_option.js
@@ -0,0 +1,214 @@
+const logotext = "DILANYA";
+const meta = {
+  title: "Dilanya Samarakoon",
+  description:
+    "I’m Dilanya Samarakoon designer, devloper and a photographer",
+};
+
+const introdata = {
+  title: "I’m Dilanya Samarakoon",
+  animated: {
+    first: "I love coding",
+    second: "I love designing",
+    third: "I love photography",
+  },
+  description:
+  "I’m Dilanya Samarakoon designer, devloper and a photographer",
+  your_img_url: "https://i.imgur.com/DpG28Va.png",
+};
+
+const dataabout = {
+  title: "abit about my self",
+  aboutme:
+    "I'm Dilanya Samarakoon, I am a young enthusiastic individual who seeks for the opportunity. I have been doing my studies well up to now and I have the ultimate intension of continuing doing my studies well. Currently I'm studying as an undergraduate at university of Sri Jayewardenepura.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Designer of week",
+    where: "YAdfi",
+    date: "2020",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+  },
+];
+
+const skills = [
+  {
+    name: "Python",
+    value: 80,
+  },
+  {
+    name: "java",
+    value: 85,
+  },
+  {
+    name: "Javascript",
+    value: 60,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "MongoDB",
+    value: 65,
+  },
+  {
+    name: "Photoshop skills",
+    value: 75,
+  },
+  {
+    name: "video editing",
+    value: 75,
+  },
+
+];
+
+const services = [
+  {
+    title: "University of Sri Jayewardenepura",
+    description:
+      "Bachelor of Science in Information and Com.Technology (UG)",
+  },
+  {
+    title: "Esoft Metro Campus",
+    description:
+      "Diploma in English",
+  },
+  {
+    title: "Sanghabodhi College, Nittambuwa",
+    description:
+      "GCE A/L - 2018",
+  },
+  
+];
+const Memberships = [
+  {
+    title: "Institute of Electrical and Electronics Engineers (IEEE)",
+    description:
+      "Since 2021",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://i.imgur.com/s4fo0e7.png",
+    desctiption:
+      "React-Portfolio",
+    link: "#",
+  },
+  {
+    img: "https://i.imgur.com/9T4To7s.png",
+    desctiption:
+      "Hospital Chat App",
+    link: "https://github.com/FHSS-USJ/final-project-Dilanya-1",
+  },
+  {
+    img: "https://i.imgur.com/d8CsSUa.png",
+    desctiption:
+      "Hotel Reservation system",
+    link: "https://github.com/FHSS-USJ/final-project-Dilanya",
+  }];
+  const designs =[
+  {
+    img: "https://i.imgur.com/cFRG5db.png",
+    desctiption:
+      "Hospital Chat App UI",
+    link: "#",
+  }];
+  const photos=[
+  {
+    img: "https://i.imgur.com/7CaovQC.jpg",
+    desctiption:
+      "#",
+    link: "https://i.imgur.com/7CaovQC.jpg",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://i.imgur.com/KFj4Zmv.jpg",
+    desctiption:
+      "#",
+    link: "https://i.imgur.com/KFj4Zmv.jpg",
+  },
+  {
+    img: "https://i.imgur.com/rnSRuJ7.jpg",
+    desctiption:
+      "#",
+    link: "https://i.imgur.com/rnSRuJ7.jpg",
+  },
+  {
+    img: "https://i.imgur.com/dzWFc8n.jpg",
+    desctiption:
+      "#",
+    link: "https://i.imgur.com/dzWFc8n.jpg",
+  },
+  {
+    img: "https://i.imgur.com/ncNHZSr.jpg",
+    desctiption:
+      "#",
+    link: "https://i.imgur.com/ncNHZSr.jpg",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    desctiption:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  OFFICIAL_EMAIL: "dilanyasamarakoon@ieee.org",
+  PERSONAL_EMAIL: "dilanyasamarakoon@gmail.com",
+  UNIVERSITY_EMAIL: "96423@fhss.sjp.ac.lk",
+  YOUR_FONE:"(+94) 77 81 06 367",
+              
+  description:
+    "No.481, Pannila, Ruggahawila",
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/Dilanya",
+  facebook: "https://www.facebook.com/dilanyasamarakoon",
+  linkedin: "http://www.linkedin.com/in/dilanya-samarakoon-04568021a",
+  twitter: "https://twitter.com",
+  youtube: "https://youtube.com",
+  twitch: "https://twitch.com",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+  Memberships,
+  designs,
+  photos,
+};
